a9080a93211d12a6b9e40ce8ff033714ec32bb0a
TymofiiYerofiev <tymofii.yerofiev@lotusflare.com>
2019-11-24 15:49:09 +0200

    Oh..

--- libmx/src/mx_itoa.c
+++ libmx/src/mx_itoa.c
@@ -3,11 +3,17 @@
 char *mx_itoa(int number) {
     int size = 0;
     char *result;
+    int costil = 1;
 
     for (int i = number; i != 0; i /= 10, size++);
-    result = malloc(size * sizeof(char));
-    result[size] = '\0';
-    for (int i = size - 1; i >= 0; i--, number /= 10)
-        result[i] = number % 10 + 48;
+    if (number < 0) {
+        size++;
+        costil = -1;
+    }
+        result = mx_strnew(size);
+    for (int i = size - 1; i >= 0; i--, number /= 10) {
+        result[i] = number % 10 * costil + 48;
+    }
+    result[0] = '-';
     return result;
 } 
